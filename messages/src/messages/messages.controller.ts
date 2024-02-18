7bcd4bc419eca40c46ae3e0ce052262323107c70
venkata-naveen-varma <venkata2naveen@gmail.com>
2024-02-18 10:54:32 -0500

    messages app module, controller and validation

---
+++ messages/src/messages/messages.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+
+@Controller('messages')
+export class MessagesController {
+    @Get()
+    listMessages(){
+
+    }
+    @Post()
+    createMessage(@Body() body: CreateMessageDto){
+
+        console.log(body);
+    }
+    @Get('/:id')
+    getMessage(@Param() id: String){
+        console.log(id)
+    }
+}
